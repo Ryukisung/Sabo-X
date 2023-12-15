125f25567e701528b03cd589677ef9b2e41d3460
JenjiK <153891886+JenjiK@users.noreply.github.com>
2023-12-15 14:08:10 +0700

    เพิ่มฟังก์ชันตูณหาร

--- calculate.py
+++ calculate.py
@@ -1,3 +1,34 @@
+def add_numbers(x, y):
+    return x + y
+
+def subtract_numbers(x, y):
+    return x - y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def divide_numbers(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-sol = input("Enter add or sub , mul or div: ")
+
+operation = input("Enter 'add' for addition, 'sub' for subtraction, 'mul' for multiplication, or 'div' for division: ")
+
+if operation == 'add':
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
+elif operation == 'sub':
+    result = subtract_numbers(num1, num2)
+    print(f"The difference between {num1} and {num2} is: {result}")
+elif operation == 'mul':
+    result = multiply_numbers(num1, num2)
+    print(f"The product of {num1} and {num2} is: {result}")
+elif operation == 'div':
+    result = divide_numbers(num1, num2)
+    print(f"The quotient of {num1} divided by {num2} is: {result}")
+else:
+    print("Invalid operation. Please enter 'add', 'sub', 'mul', or 'div'.")
