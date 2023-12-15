d870513393e694e17570654031d2ed1757c0300a
65160053 <153892268+65160053@users.noreply.github.com>
2023-12-15 14:12:04 +0700

    DVe 2

--- calculate.py
+++ calculate.py
@@ -1,3 +1,10 @@
+def multiply_numbers(x, y):
+    return x * y
+    
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 sol = input("Enter add or sub , mul or div: ")
+
+elif operation == 'mul':
+    result = multiply_numbers(num1, num2)
+    print(f"The product of {num1} and {num2} is: {result}")
